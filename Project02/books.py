87bc07c4790f3cf57763ebda59784be0f0b82edb
Okan Hollander <okan.hollander@quality.nl>
2024-03-09 19:51:13 +0100

    updated post validation

--- Project02/books.py
+++ Project02/books.py
@@ -1,5 +1,6 @@
-from fastapi import Body, FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -19,11 +20,11 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    book_id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+    book_id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
 
 BOOKS = [
     Book(1, "Computer Science Pro", "Dramaqueen", "A very nice book!", 5),
@@ -34,6 +35,11 @@ BOOKS = [
     Book(6, "HP3", "Dramaqueen", "Book description", 1),
 ]
 
+# find book id
+def find_book_id(book: Book):
+    book.book_id = 1 if len(BOOKS) == 0 else BOOKS[-1].book_id + 1
+    return book
+
 @app.get("/books")
 async def read_all_books():
     return BOOKS
@@ -42,5 +48,5 @@ async def read_all_books():
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
-    BOOKS.append(new_book)
+    BOOKS.append(find_book_id(new_book))
     
