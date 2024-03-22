75b8ee73e51f932a756a3f14568ea53a0775690a
Okan Hollander <okan.hollander@quality.nl>
2024-03-22 11:08:39 +0100

    implemented authentication

--- Project03/routers/auth.py
+++ Project03/routers/auth.py
@@ -6,6 +6,7 @@ from database import SessionLocal
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -30,6 +31,15 @@ def get_db():
 
 db_dependancy = Annotated[Session, Depends(get_db)]
 
+def auth_user(username: str,
+              password: str,
+              db):
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependancy,
@@ -45,3 +55,12 @@ async def create_user(db: db_dependancy,
     )
     db.add(create_user_model)
     db.commit()
+
+@router.post("/auth/login", status_code=status.HTTP_200_OK)
+async def login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                     db: db_dependancy):
+    user = auth_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed to authenticate'
+
+    return 'Successfully logged in'
