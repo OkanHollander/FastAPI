5a754ebcdac4ec0d44e444ceeb0e0aaace991748
Okan Hollander <okan.hollander@quality.nl>
2024-03-22 10:39:39 +0100

    implemented create user

--- Project03/routers/auth.py
+++ Project03/routers/auth.py
@@ -1,7 +1,28 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from models import User
 
 router = APIRouter()
 
-@router.get("/auth")
-async def get_user():
-    return {'user': 'authenticated'}
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+@router.post("/auth")
+async def create_user(CreateUserRequest: CreateUserRequest):
+    create_user_model = User(
+        email=CreateUserRequest.email,
+        username=CreateUserRequest.username,
+        first_name=CreateUserRequest.first_name,
+        last_name=CreateUserRequest.last_name,
+        hashed_password=CreateUserRequest.password,
+        role=CreateUserRequest.role,
+        is_active=True,
+    )
+    return create_user_model
