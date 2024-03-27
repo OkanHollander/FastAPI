b2eb480ac9e106629217ac96d661de16d493eddd
Okan Hollander <okan.hollander@quality.nl>
2024-03-27 12:34:04 +0100

    updated admin user role rights

--- Project03/routers/auth.py
+++ Project03/routers/auth.py
@@ -55,9 +55,11 @@ def auth_user(username: str,
 
 def create_access_token(username: str,
                         user_id: int,
+                        role: str,
                         expires_delta: timedelta):
     encode = {'sub': username,
-              'id': user_id}
+              'id': user_id,
+              'role': role,}
     expired = datetime.utcnow() + expires_delta
     encode.update({'exp': expired})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -67,9 +69,10 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get('sub')
         user_id: int = payload.get('id')
+        user_role: str = payload.get('role')
         if username is None or user_id is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
-        return {'username': username, 'id': user_id}
+        return {'username': username, 'id': user_id, 'user_role': user_role}
     except JWTError as exc:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate credentials') from exc
 
@@ -98,6 +101,7 @@ async def login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
     token = create_access_token(user.username,
                                 user.id,
+                                user.role,
                                 timedelta(minutes=20))
 
     return {'access_token': token, 'token_type': 'bearer'}
