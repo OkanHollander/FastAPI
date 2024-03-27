f233a47051a2bb0f33a45eb032821ea2883aaaec
Okan Hollander <okan.hollander@quality.nl>
2024-03-27 20:40:33 +0100

    updated with pytest objects

--- Project04/test/test_example.py
+++ Project04/test/test_example.py
@@ -1,3 +1,5 @@
+import pytest
+
 def test_equal_or_not_equal():
     assert 3 == 3
     assert 3 != 1
@@ -26,3 +28,22 @@ def test_greater_and_less_than():
 def test_list():
     num_list = [1, 2, 3, 4, 5]
     assert num_list[0] == 1
+
+class Student:
+    def __init__(self, first_name: str, last_name: str, major: str, years: int):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.major = major
+        self.years = years
+
+
+@pytest.fixture
+def default_employee():
+    return Student('John', 'Doe', 'Computer Science', 3)
+
+
+def test_person_init(default_employee):
+    assert default_employee.first_name == 'John'
+    assert default_employee.last_name == 'Doe'
+    assert default_employee.major == 'Computer Science'
+    assert default_employee.years == 3
