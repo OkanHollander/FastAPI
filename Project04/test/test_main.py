5324a0159fcab2060379dd1ffefd45896fc4d3d2
Okan Hollander <okan.hollander@quality.nl>
2024-03-28 07:56:01 +0100

    updated imports with relative path

--- Project04/test/test_main.py
+++ Project04/test/test_main.py
@@ -1,8 +1,8 @@
 from fastapi.testclient import TestClient
 from fastapi import status
-import main
+from ..main import app
 
-client = TestClient(main.app)
+client = TestClient(app)
 
 def test_return_health_check():
     response = client.get('/healthy')
