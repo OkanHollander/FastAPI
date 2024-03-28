869e79c8677ba1270ea20d18d8493825df479319
Okan Hollander <okan.hollander@quality.nl>
2024-03-28 13:44:43 +0100

    implemented test for delete and not found

--- Project04/test/test_admin.py
+++ Project04/test/test_admin.py
@@ -1,6 +1,7 @@
 from fastapi import status
 from .utils import *
 from ..routers.admin import get_db, get_current_user
+from ..models import Todos
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
@@ -29,3 +30,43 @@ def test_admin_read_all_authenticated(test_todo):
             "owner_id": 1,
         }
     ]
+
+def test_admin_delete_todo(test_todo):
+    """
+    Test that authenticated users can delete a todo.
+
+    Args:
+        test_todo (object): A test todo object.
+
+    Returns:
+        None
+
+    """
+    # Make a request to delete a todo
+    response = client.delete('/admin/todos/1')
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 1).first()
+
+    assert model is None
+
+def test_admin_delete_todo_not_found(test_todo):
+    """
+    Test that authenticated users can delete a todo.
+
+    Args:
+        test_todo (object): A test todo object.
+
+    Returns:
+        None
+
+    """
+    # Make a request to delete a todo
+    response = client.delete('/admin/todos/100')
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 100).first()
+
+    assert model is None
